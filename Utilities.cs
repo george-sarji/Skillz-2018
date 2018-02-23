6a53d3b9ec4fbca48762bf667c6f5639dc871475
george17-meet <george17@meet.mit.edu>
2018-02-23 10:58:15 +0200

    Added utilities class.

---
+++ Utilities.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using Pirates;
+
+namespace Skillz_Code
+{
+    partial class SSJS12Bot : IPirateBot
+    {
+        private static int DistanceThroughWormhole(Location from, MapObject to, Wormhole wormhole, IEnumerable<Wormhole> wormholes)
+        {
+            return from.Distance(wormhole) +
+                ClosestDistance(wormhole.Partner.Location, to,
+                    wormholes.Where(w => w.Id != wormhole.Id && w.Id != wormhole.Partner.Id));
+        }
+
+        private static int ClosestDistance(Location from, MapObject to, IEnumerable<Wormhole> wormholes)
+        {
+            if (wormholes.Any())
+            {
+                int distanceWithoutWormholes = from.Distance(to);
+                int distanceWithWormholes = wormholes
+                    .Select(wormhole => DistanceThroughWormhole(from, to, wormhole, wormholes))
+                    .Min();
+                return System.Math.Min(distanceWithoutWormholes, distanceWithWormholes);
+            }
+            return from.Distance(to);
+        }
+
+    }
+}
