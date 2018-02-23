bf0543854b97e833b5c699b9783c987ad8da5e54
mahmoud17-meet <mahmoud17-meet@mit.edu>
2018-02-23 11:49:22 +0200

    Added new class called capsule

---
+++ Capsules.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
+using Pirates;
+
+namespace Skillz_Code {
+    partial class SSJS12Bot : IPirateBot {
+        public void CaptureCapsules () 
+        {
+            foreach (Capsule capsule in myCapsules)
+            {
+                
+            }
+        }
+    }
+}
